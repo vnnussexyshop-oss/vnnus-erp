2aab23f0a91da397fa2913cb654a11b7074d83df
vnnussexyshop-oss <vnnus.sexyshop@gmail.com>
2026-08-10 20:48:13 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,177 @@
-const CACHE="vnnus-erp-v1";
-const ASSETS=["./", "./index.html", "./css/app.css", "./js/config.js", "./js/api.js", "./js/router.js", "./js/app.js", "./pages/dashboard.html", "./pages/produtos.html", "./pages/scanner.html", "./pages/estoque.html", "./pages/pdv.html", "./pages/clientes.html", "./pages/financeiro.html", "./pages/configuracoes.html", "./assets/icons/icon-192.png", "./assets/icons/icon-512.png", "./manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./index.html")))));
+const CACHE =
+  'vnnus-erp-v2';
+
+
+const ASSETS = [
+
+  './',
+
+  './index.html',
+
+  './css/app.css',
+
+  './js/config.js',
+
+  './js/api.js',
+
+  './js/router.js',
+
+  './js/app.js',
+
+  './pages/dashboard.html',
+
+  './pages/produtos.html',
+
+  './pages/scanner.html',
+
+  './pages/estoque.html',
+
+  './pages/pdv.html',
+
+  './pages/clientes.html',
+
+  './pages/financeiro.html',
+
+  './pages/configuracoes.html',
+
+  './assets/icons/icon-192.png',
+
+  './assets/icons/icon-512.png',
+
+  './manifest.webmanifest'
+
+];
+
+
+self.addEventListener(
+  'install',
+  function(event) {
+
+    self.skipWaiting();
+
+    event.waitUntil(
+
+      caches
+        .open(CACHE)
+        .then(
+          function(cache) {
+
+            return cache.addAll(
+              ASSETS
+            );
+
+          }
+        )
+
+    );
+
+  }
+);
+
+
+self.addEventListener(
+  'activate',
+  function(event) {
+
+    event.waitUntil(
+
+      caches
+        .keys()
+        .then(
+          function(keys) {
+
+            return Promise.all(
+
+              keys
+                .filter(
+                  function(key) {
+
+                    return (
+                      key !== CACHE
+                    );
+
+                  }
+                )
+                .map(
+                  function(key) {
+
+                    return caches.delete(
+                      key
+                    );
+
+                  }
+                )
+
+            );
+
+          }
+        )
+        .then(
+          function() {
+
+            return self.clients.claim();
+
+          }
+        )
+
+    );
+
+  }
+);
+
+
+self.addEventListener(
+  'fetch',
+  function(event) {
+
+    /*
+      Para HTML, JS e CSS:
+      tenta sempre pegar a versão nova primeiro.
+    */
+
+    event.respondWith(
+
+      fetch(
+        event.request
+      )
+
+      .then(
+        function(response) {
+
+          const copia =
+            response.clone();
+
+
+          caches
+            .open(CACHE)
+            .then(
+              function(cache) {
+
+                cache.put(
+                  event.request,
+                  copia
+                );
+
+              }
+            );
+
+
+          return response;
+
+        }
+      )
+
+      .catch(
+        function() {
+
+          return caches.match(
+            event.request
+          );
+
+        }
+      )
+
+    );
+
+  }
+);
