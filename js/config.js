1c2f490bc9ed753d06c3f45fa40976c9f187310a
vnnussexyshop-oss <vnnus.sexyshop@gmail.com>
2026-08-26 14:58:44 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window.VNNUS_CONFIG = {
 
   API_URL:
-  'https://script.google.com/macros/s/AKfycbwHNdcYei0oPwOSFzKYOvB9NgOjf16SA-SLoCt_jSjs8mOH_kj1KbrZV5pewxozZsmCsQ/exec',
+  'https://script.google.com/macros/s/AKfycbz5oCbPh8TdnWQzSzZxNzvHhuykx131Y037GfyHNlM-tmxElUs0acLJcbDWxBhfJp3kMg/exec',
 
   SCANNER_URL:
   'https://vnnussexyshop-oss.github.io/vnnus-scanner/'
